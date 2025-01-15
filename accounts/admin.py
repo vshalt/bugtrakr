110574b268ad56e615b816ffd4e5802fcbb07ced
vishal <chocotacove@gmail.com>
2025-01-16 01:04:41 +0530

    fix admin page

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Role
 
 
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'github', 'roles')
+admin.site.register(Profile)
+
+admin.site.register(Role)
