ff8dc9ae4511979d54518b6a31a59301fc895ab2
vishal <chocotacove@gmail.com>
2025-01-15 23:50:23 +0530

    update roles field

--- accounts/models.py
+++ accounts/models.py
@@ -2,24 +2,24 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
-from multiselectfield import MultiSelectField
 
 User = get_user_model()
 
+class Role(models.Model):
+    role = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.role}"
 
 class Profile(models.Model):
-    choices = (('submitter', 'Submitter'),
-               ('developer', 'Developer'),
-               ('manager', 'Manager'),
-               ('admin', 'Admin'))
-
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     github = models.CharField(max_length=80, blank=True)
-    roles = MultiSelectField(choices=choices, default='submitter')
+    roles = models.ManyToManyField(Role)
 
     def __str__(self):
         return self.user.__str__()
 
     def get_absolute_url(self):
         return reverse('user_detail', args=[self.id])
+
